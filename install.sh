16e7ea6d181d95875b82dc8314be8592a30e810b
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-04-05 11:01:25 -0400

    Cleaned up install.sh
    DRY, and stability if you run it from another directory

--- install.sh
+++ install.sh
@@ -1,42 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+
+DIR=`readlink -f ${BASH_SOURCE[0]}`
 
 echo "Installing all Emacs-related files from the kotct.emacs project to \$HOME"
 
-if test -e "$HOME/.emacs";
-then
-	printf "\"$HOME/.emacs\" already exists and is a "
-
-	if test -h "$HOME/.emacs";
+for file in .emacs .emacs.d;
+do
+	if test -e "$HOME/$file";
 	then
-		printf "symbolic link.\n"
-	else
-		printf "non-symbolic file.\n"
+		printf "\"$HOME/$file\" already exists and is "
+
+		if test -h "$HOME/$file";
+		then
+			printf "a symbolic link.\n"
+		elif test -d "$HOME/$file";
+		then
+			printf "a directory.\n"
+		elif test -f "$HOME/$file";
+		then
+			printf "a regular file.\n"
+		else
+			printf "some sort of weird voodoo magic.\n"
+		fi
+
+		printf "We don't want to clobber your existing configuration, so please remove ~/$file and try again.\n"
+		printf "If you encounter the same issue again, please submit an issue on the issue tracker at:\n"
+		printf "https://github.com/kotct/kotct.emacs/issues\n"
+
+		exit
 	fi
+done
 
-	printf "We don't want to clobber your existing configuration, so please remove those files and try again.\n"
-	printf "If you encounter the same issue again, please submit an issue on the issue tracker at:\n"
-	printf "https://github.com/kotct/kotct.emacs/issues\n"
-
-	exit
-fi
-
-if test -e "$HOME/.emacs.d";
-then
-	printf "\"$HOME/.emacs.d\" already exists and is a "
-
-	if test -h "$HOME/.emacs.d";
-	then
-		printf "symbolic link.\n"
-	else
-		printf "non-symbolic file.\n"
-	fi
-
-	printf "We don't want to clobber your existing configuration, so please remove those files and try again.\n"
-	printf "If you encounter the same issue again, please submit an issue on the issue tracker at:\n"
-	printf "https://github.com/kotct/kotct.emacs/issues\n"
-
-	exit
-fi
-
-ln -s `pwd`/.emacs $HOME/.emacs
-ln -s `pwd`/.emacs.d $HOME/.emacs.d
+ln -s $DIR/.emacs $HOME/.emacs
+ln -s $DIR/.emacs.d $HOME/.emacs.d
