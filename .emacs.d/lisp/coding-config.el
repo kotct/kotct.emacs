a808c2434654f7bfc75ca1b7077e736aa43c8893
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-07-24 18:21:03 -0400

    Actually make C-M-w work like it's supposed to.

--- .emacs.d/lisp/coding-config.el
+++ .emacs.d/lisp/coding-config.el
@@ -2,16 +2,19 @@
 (delete-selection-mode t)
 
 ;; Copy sexp as kill
+(setq *sexp-copy-count* 0)
+(defun count-commands () (setq *sexp-copy-count* (+ 1 *sexp-copy-count*)))
+(add-hook 'pre-command-hook 'count-commands)
 (defun copy-sexp-as-kill (arg)
   "Save sexp as if it were killed, but don't kill it."
   (interactive "p")
   (let ((beg (point)))
     (let ((parse-sexp-ignore-comments t))
       (forward-sexp arg))
-    (if (= 1 *rlg-count*)
+    (if (= 1 *sexp-copy-count*)
         (append-next-kill))
     (clipboard-kill-ring-save beg (point))
-    (setq *rlg-count* 0)))
+    (setq *sexp-copy-count* 0)))
 
 (global-set-key "\e\^W" 'copy-sexp-as-kill)
 
