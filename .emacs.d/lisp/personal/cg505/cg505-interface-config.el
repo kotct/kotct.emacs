b1e43092dbe5d9c2a9369ca7596e95924ea4724e
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-07-27 14:26:10 -0400

    [cg505-config] Smex on M-x

--- .emacs.d/lisp/personal/cg505/cg505-interface-config.el
+++ .emacs.d/lisp/personal/cg505/cg505-interface-config.el
@@ -4,12 +4,6 @@
                             (thing (concat " %" (number-to-string (max 2 w)) "d ")))
                        (propertize (format thing line) 'face 'linum))))
 
-;; windmove keybindings with shift
-(global-set-key (kbd "C-S-i") 'windmove-up)
-(global-set-key (kbd "C-S-j") 'windmove-left)
-(global-set-key (kbd "C-S-k") 'windmove-down)
-(global-set-key (kbd "C-S-l") 'windmove-right)
-
 ;; woah
 (load-theme 'cyberpunk)
 
