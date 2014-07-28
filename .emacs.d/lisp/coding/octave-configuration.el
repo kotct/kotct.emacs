a27f517dbe2ba74ccb1b0d72b19be16a01e596bb
Christopher Cooper <christopher.c.cooper@gmail.com>
2014-07-28 07:17:03 -0400

    lol i'm stupid

--- .emacs.d/lisp/coding/octave-configuration.el
+++ .emacs.d/lisp/coding/octave-configuration.el
@@ -1,5 +1,5 @@
 ;; Load octave-mode on *.m files
 (autoload 'octave-mode "octave-mod" nil t)
-(add-to-list auto-mode-alist '("\\.m$" . octave-mode))
+(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
 
 (provide 'octave-configuration)
