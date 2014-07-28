b38b636151d01e6606aaf584c7e5da0d093449b4
samercier <merciers.merciers@gmail.com>
2014-07-28 09:47:37 -0400

    Fixing things because people lie

--- .emacs.d/lisp/personal/samontea/samontea-theme-configuration.el
+++ .emacs.d/lisp/personal/samontea/samontea-theme-configuration.el
@@ -1,9 +1,24 @@
-(setq monokai-height-minus-1 1
-      monokai-height-plus-1  1
-      monokai-height-plus-2  1
-      monokai-height-plus-3  1
-      monokai-height-plus-4  1)
+(defcustom monokai-height-plus-4 1.0
+             "ALL THE SAME"
+             :type 'number
+             :group 'monokai)
+(defcustom monokai-height-plus-3 1.0
+             "ALL THE SAME"
+             :type 'number
+             :group 'monokai)
+(defcustom monokai-height-plus-2 1.0
+             "ALL THE SAME"
+             :type 'number
+             :group 'monokai)
+(defcustom monokai-height-plus-1 1.0
+             "ALL THE SAME"
+             :type 'number
+             :group 'monokai)
+(defcustom monokai-height-minus-1 1.0
+             "ALL THE SAME"
+             :type 'number
+             :group 'monokai)
 
-(load-theme 'monokai 'no-confirm)
+(load-theme 'monokai)
 
 (provide 'samontea-theme-configuration)
