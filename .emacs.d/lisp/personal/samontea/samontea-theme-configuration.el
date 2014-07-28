691d26e1a3cc11baf6e82e7084822cb5a02edf1b
samercier <merciers.merciers@gmail.com>
2014-07-28 09:29:22 -0400

    something

---
+++ .emacs.d/lisp/personal/samontea/samontea-theme-configuration.el
@@ -0,0 +1,9 @@
+(setq monokai-height-minus-1 1
+      monokai-height-plus-1  1
+      monokai-height-plus-2  1
+      monokai-height-plus-3  1
+      monokai-height-plus-4  1)
+
+(load-theme 'monokai 'no-confirm)
+
+(provide 'samontea-theme-configuration)
