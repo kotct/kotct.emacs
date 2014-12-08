092fbc16bdc5795d067e882afc66ac36eb407613
samercier <merciers.merciers@gmail.com>
2014-12-08 15:49:27 -0500

    show paren, changed help key combo, and other personal stuffz

--- .emacs.d/lisp/personal/samontea/samontea-keybindings-configuration.el
+++ .emacs.d/lisp/personal/samontea/samontea-keybindings-configuration.el
@@ -4,6 +4,13 @@
 (global-set-key (kbd "C-S-h") 'windmove-left)
 (global-set-key (kbd "C-S-l") 'windmove-right)
 
+;; For natural movement between splits in terminals
+(global-set-key (kbd "C-x M-k") 'windmove-up)
+(global-set-key (kbd "C-x M-j") 'windmove-down)
+(global-set-key (kbd "C-x M-h") 'windmove-left)
+(global-set-key (kbd "C-x M-l") 'windmove-right)
+
+
 ;o like functionality from vim, really helpful in org-mode
 (global-set-key (kbd "C-o") 'newline)
 
@@ -13,6 +20,6 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;HELP! I need somebody HELP! not just anybody
-(global-set-key (kbd "C-h") 'help-command)
+(global-set-key (kbd "C-x h") 'help-command)
 
 (provide 'samontea-keybindings-configuration)
