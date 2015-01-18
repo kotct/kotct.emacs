817c7848a11ef0ef62146a9e122c54d2ffc59bdf
Christopher Cooper <christopher.c.cooper@gmail.com>
2015-01-17 20:31:28 -0600

    make SP disable on emacs-lisp-mode load actually work

--- .emacs.d/lisp/non-global/user/cg505/cg505-paredit-config.el
+++ .emacs.d/lisp/non-global/user/cg505/cg505-paredit-config.el
@@ -2,8 +2,8 @@
 
 ;; Use paredit mode, not smartparens
 (defun switch-to-paredit ()
-  (paredit-mode t)
-  (smartparens-mode nil))
+  (paredit-mode 1)
+  (smartparens-mode 0))
 (add-hook 'lisp-mode-hook 'switch-to-paredit)
 (add-hook 'emacs-lisp-mode-hook 'switch-to-paredit)
 
