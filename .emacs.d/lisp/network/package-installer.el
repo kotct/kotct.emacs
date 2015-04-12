db8475ea6d0be408da18490e9b2b022d06c28877
Christopher Cooper <christopher.c.cooper@gmail.com>
2015-04-12 16:10:22 -0400

    Pass an arg to package installer to skip archive refresh
    C-u M-x package-installer-install-dependencies will not
    refresh the package archive. Also will print a message
    on completion indicating whether or not packages were installed.

--- .emacs.d/lisp/network/package-installer.el
+++ .emacs.d/lisp/network/package-installer.el
@@ -1,17 +1,24 @@
-(defun package-installer-install-dependencies ()
-  "Installs the dependencies"
-  (interactive "*")
+(defun package-installer-install-dependencies (arg &optional update)
+  "Installs the dependencies.
+With a non-nil or prefix ARG, do not refresh package list."
+  (interactive "P")
 
-  (progn
-    (require 'dependencies)
+  (require 'dependencies)
 
-    (package-refresh-contents)
+  (unless arg (package-refresh-contents))
 
+  (let ((found nil))
     (mapc
      (lambda (package)
        (if (not (package-installed-p package))
-           (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-               (package-install package))))
-     dependency-list)))
+           (progn
+             (setq found t)
+             (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+                 (package-install package)))))
+     dependency-list)
+
+    (if found
+        (message "Dependency installation completed.")
+      (message "No dependencies needing installation."))))
 
 (provide 'package-installer)
