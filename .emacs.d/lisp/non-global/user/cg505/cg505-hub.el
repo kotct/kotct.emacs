6a1e13390f82c1602e2189eb99b34a5e239ab333
Christopher Cooper <christopher.c.cooper@gmail.com>
2015-04-14 19:10:16 -0400

    [personal] mode-line config except not

--- .emacs.d/lisp/non-global/user/cg505/cg505-hub.el
+++ .emacs.d/lisp/non-global/user/cg505/cg505-hub.el
@@ -3,12 +3,14 @@
 (require 'cg505-coding-config)
 (require 'cg505-interface-config)
 (require 'cg505-keybindings)
+(require 'cg505-mode-line-configuration)
 
 (defun cg505-hub-unload-function ()
   (unload-feature 'cg505-paredit-config 'force)
   (unload-feature 'cg505-coding-config 'force)
   (unload-feature 'cg505-interface-config 'force)
   (unload-feature 'cg505-keybindings 'force)
-  (unload-feature 'cg505-lib 'force))
+  (unload-feature 'cg505-lib 'force)
+  (unload-feature 'cg505-mode-line-configuration 'force))
 
 (provide 'cg505-hub)
---
+++ .emacs.d/lisp/non-global/user/cg505/cg505-mode-line-configuration.el
@@ -0,0 +1,33 @@
+;; (defun mode-line-configuration-internal--set-mode-line-format ()
+;;   "Sets the mode-line-format to the value that it should be set to globally."
+
+;;   (interactive)
+;;   (progn
+;;     (setq-default mode-line-format
+;;                   '("%e"
+;;                     mode-line-mule-info
+;;                     mode-line-client
+;;                     "<%*%+>"
+;;                     "%@"
+;;                     mode-line-frame-identification
+;;                     (#("%16b" 0 4 (local-map mode-line-highlight face mode-line-buffer-id)))
+;;                     " "
+;;                     "(%l,%c)"
+;;                     " "
+;;                     (vc-mode vc-mode)
+;;                     " "
+;;                     mode-line-misc-info))));
+
+;; ;; Modeline battery notifier config & enabling
+;; (setq-default battery-mode-line-format "[%b%p%%, %t] ")
+;; (display-battery-mode)
+
+;; ;; Modeline clock config & enabling
+;; (setq display-time-format "{%Y-%m-%d, %H:%M}")
+;; (setq display-time-default-load-average nil)
+;; (display-time-mode)
+
+;; ;; Enable the mode line config
+;; ;; (mode-line-configuration-internal--set-mode-line-format)
+
+(provide 'cg505-mode-line-configuration)
