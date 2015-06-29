754f12b0f5a58cf88493c543f3791aec2df3495b
samontea <merciers.merciers@gmail.com>
2015-06-29 14:39:49 -0500

    added auto-install functionality for update funct

--- .emacs.d/lisp/network/package-installer.el
+++ .emacs.d/lisp/network/package-installer.el
@@ -1,7 +1,7 @@
 (defun package-up-to-date (package)
   (every (lambda (x) (package-installed-p package (package-desc-version x))) (cdr (assq package package-archive-contents))))
 
-(defun package-installer-install-dependencies (arg &optional update)
+(defun package-installer-install-dependencies (arg &optional update auto-install)
   "Installs the dependencies.
 With a non-nil or prefix ARG, do not refresh package list.
 If UPDATE is non-nil, out-of-date packages will be updated."
@@ -18,11 +18,16 @@ If UPDATE is non-nil, out-of-date packages will be updated."
         (if (or (not (package-installed-p package)) (and update (not (package-up-to-date package)) (setq updating t)))
             (progn
               (setq found t)
-              (if (y-or-n-p (format "Package %s is %s. Install it? " package (if updating "out of date" "missing")))
+              (if auto-install
                   (add-to-list 'install-list (cons package
-                                                   ;; haxily say we need the next version by adding a .1 to the version
-                                                   ;; ie if we have version 2.3.0 ask for 2.3.0.1
-                                                   (if updating (list (append (package-desc-version (cadr (assq package package-alist))) '(1)))))))))))
+                                                       ;; haxily say we need the next version by adding a .1 to the version
+                                                       ;; ie if we have version 2.3.0 ask for 2.3.0.1
+                                                       (if updating (list (append (package-desc-version (cadr (assq package package-alist))) '(1))))))
+                  (if (y-or-n-p (format "Package %s is %s. Install it? " package (if updating "out of date" "missing")))
+                      (add-to-list 'install-list (cons package
+                                                       ;; haxily say we need the next version by adding a .1 to the version
+                                                       ;; ie if we have version 2.3.0 ask for 2.3.0.1
+                                                       (if updating (list (append (package-desc-version (cadr (assq package package-alist))) '(1))))))))))))
 
     (if found
         (progn (package-download-transaction (package-compute-transaction () install-list))
@@ -35,6 +40,6 @@ using `package-installer-install-dependencies'.
 With an argument, do not refresh package list"
   (interactive "P")
 
-  (package-installer-install-dependencies arg t))
+    (package-installer-install-dependencies arg t (y-or-n-p "Auto install and update all packages?")))
 
 (provide 'package-installer)
