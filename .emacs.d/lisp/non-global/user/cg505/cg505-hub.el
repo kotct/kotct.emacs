3dae64bff22ff261f3760fd8cf0ffc104fe56da1
Christopher Cooper <christopher.c.cooper@gmail.com>
2015-07-03 19:11:50 -0400

    [personal] move theme stuff to own file

--- .emacs.d/lisp/non-global/user/cg505/cg505-hub.el
+++ .emacs.d/lisp/non-global/user/cg505/cg505-hub.el
@@ -2,6 +2,7 @@
 (require 'cg505-paredit-config)
 (require 'cg505-coding-config)
 (require 'cg505-interface-config)
+(require 'cg505-theme-config)
 (require 'cg505-keybindings)
 (require 'cg505-mode-line-configuration)
 
@@ -9,6 +10,7 @@
   (unload-feature 'cg505-paredit-config 'force)
   (unload-feature 'cg505-coding-config 'force)
   (unload-feature 'cg505-interface-config 'force)
+  (unload-feature 'cg505-theme-config 'force)
   (unload-feature 'cg505-keybindings 'force)
   (unload-feature 'cg505-lib 'force)
   (unload-feature 'cg505-mode-line-configuration 'force))
