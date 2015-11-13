84d377cb88cf01fc43a38bd36f4225b7d36b63f3
samontea <merciers.merciers@gmail.com>
2015-11-13 00:54:10 -0500

    make recursive make buffer more clear

--- .emacs.d/lisp/coding/recursive-make-configuration.el
+++ .emacs.d/lisp/coding/recursive-make-configuration.el
@@ -1,6 +1,7 @@
 (defun recursive-make ()
   "Recursively search for a make function."
   (interactive)
+  (kill-buffer "*recursive-make*")
   (start-process "recursive-make" "*recursive-make*" "~/.emacs.d/python/findmake.py" (buffer-file-name (current-buffer)))
   (display-buffer "*recursive-make*"))
 
