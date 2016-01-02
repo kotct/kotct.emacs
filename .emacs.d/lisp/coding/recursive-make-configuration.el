96ee2aad0412b498a67645c88c5419720129bcf4
samontea <merciers.merciers@gmail.com>
2016-01-02 17:59:19 -0600

    fix no *recursive-make* buffer bug

--- .emacs.d/lisp/coding/recursive-make-configuration.el
+++ .emacs.d/lisp/coding/recursive-make-configuration.el
@@ -1,7 +1,8 @@
 (defun recursive-make ()
   "Recursively search for a make function."
   (interactive)
-  (kill-buffer "*recursive-make*")
+  (if (get-buffer "*recursive-make*")
+      (kill-buffer "*recursive-make*"))
   (start-process "recursive-make" "*recursive-make*" "~/.emacs.d/python/findmake.py" (buffer-file-name (current-buffer)))
   (display-buffer "*recursive-make*"))
 
