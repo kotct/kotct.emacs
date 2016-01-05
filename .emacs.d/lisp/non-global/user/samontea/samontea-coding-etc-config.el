e31ddf3a3629d20221760cdbb9edf2b63ce0cde6
samontea <merciers.merciers@gmail.com>
2016-01-05 00:16:30 -0500

    [personal]  oops i'm bad at typing

--- .emacs.d/lisp/non-global/user/samontea/samontea-coding-etc-config.el
+++ .emacs.d/lisp/non-global/user/samontea/samontea-coding-etc-config.el
@@ -6,7 +6,7 @@
 (global-git-gutter+-mode t)
 
 (defun vim-new-line-below ()
-  "o like functionality from vim, really helpful in org-mode"n
+  "o like functionality from vim, really helpful in org-mode"
   (interactive)
   (unless (eolp)
     (end-of-line))
