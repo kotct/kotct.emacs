0ba7f5c883ab5e99852b17a47a384a04f11b6a08
Christopher Cooper <christopher.c.cooper@gmail.com>
2016-04-02 19:56:40 -0400

    add buffer-move

--- .emacs.d/lisp/interface/keybinding-configuration.el
+++ .emacs.d/lisp/interface/keybinding-configuration.el
@@ -4,6 +4,10 @@
 (global-set-key (kbd "C-s-k") 'windmove-down)
 (global-set-key (kbd "C-s-j") 'windmove-left)
 (global-set-key (kbd "C-s-;") 'windmove-right)
+(global-set-key (kbd "C-M-s-l") 'buf-move-up)
+(global-set-key (kbd "C-M-s-k") 'buf-move-down)
+(global-set-key (kbd "C-M-s-j") 'buf-move-left)
+(global-set-key (kbd "C-M-s-;") 'buf-move-right)
 (global-set-key (kbd "<f11>") 'toggle-fullscreen)
 (global-set-key (kbd "<f5>") 'recursive-make)
 
